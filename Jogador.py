e2e68e386f33e626d7c22f7b6be6f3f7de19ba63
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-02-03 02:39:43 -0300

    Revert "first commit"
    This reverts commit fa59b90d204b33a980cd8c9ff6f89a313479548a.

--- Jogador.py
+++
@@ -1,8 +0,0 @@
-class Jogador(): 
-    def __init__(self,id, name, mapa):
-        self.id = id
-        self.nome = name
-        self.mapa = mapa
-    
-    def conquistarTerritórios(): 
-        pass 
