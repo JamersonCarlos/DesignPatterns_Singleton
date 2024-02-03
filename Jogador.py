fa59b90d204b33a980cd8c9ff6f89a313479548a
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-02-03 02:38:33 -0300

    first commit

---
+++ Jogador.py
@@ -0,0 +1,8 @@
+class Jogador(): 
+    def __init__(self,id, name, mapa):
+        self.id = id
+        self.nome = name
+        self.mapa = mapa
+    
+    def conquistarTerritórios(): 
+        pass 
---
+++ Territory.py
@@ -0,0 +1,11 @@
+class Territory(): 
+    def __init__(self, identification, cordenadas):
+        self._id = identification 
+        self._cordenadas = cordenadas 
+        self._jogador = ''
+        
+    def setJogador(self, jogador): 
+        self._jogador = jogador 
+    
+    
+    
